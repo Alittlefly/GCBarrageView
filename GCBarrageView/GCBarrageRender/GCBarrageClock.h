8e9d4df5b521fedb12a431c068d7293508380854
gaochao <1176672158@qq.com>
2017-06-11 18:14:43 +0800

    timeClock

--- GCBarrageView/GCBarrageRender/GCBarrageClock.h
+++ GCBarrageView/GCBarrageRender/GCBarrageClock.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@class GCBarrageClock;
+@protocol GCBarrageClockDelegate <NSObject>
+@optional
+-(void)barrageClock:(GCBarrageClock *)clock updateTime:(NSTimeInterval)timeInterval;
+@end
 
 @interface GCBarrageClock : NSObject
 
+@property(nonatomic,assign)CGFloat timeSpeed;
+
+-(instancetype)initWithDelegate:(id<GCBarrageClockDelegate>)delegate;
+
+
+-(void)start;
+-(void)stop;
+-(void)pause;
+-(void)resume;
 @end
--- GCBarrageView/GCBarrageRender/GCBarrageClock.m
+++ GCBarrageView/GCBarrageRender/GCBarrageClock.m
@@ -7,7 +7,98 @@
 //
 
 #import "GCBarrageClock.h"
+@interface GCBarrageClock()
+{
+    CADisplayLink *_clock;
+    CFTimeInterval _previousDate; // 上一次更新时间
+    CGFloat _pausedTimeSpeed; // 暂停之前的时间流速
+}
+@property(nonatomic,assign)id<GCBarrageClockDelegate>delegate;
 
+@property(nonatomic,assign)CGFloat time;
+
+@property(nonatomic,assign)BOOL launched;
+@property(nonatomic,assign)BOOL paused;
+
+@end
 @implementation GCBarrageClock
+-(instancetype)initWithDelegate:(id<GCBarrageClockDelegate>)delegate{
+    if (self = [super init]) {
+        [self setDelegate:delegate];
+        [self initClock];
+        [self pause];
+    }
+    return self;
+}
+
+-(void)initClock{
+    if (!_clock) {
+         _clock = [CADisplayLink displayLinkWithTarget:self selector:@selector(runClock:)];
+    }
+}
+-(void)runClock:(CADisplayLink  *)clock{
+    [self updateTime];
+    
+    if ([self.delegate respondsToSelector:@selector(barrageClock:updateTime:)]) {
+        [self.delegate barrageClock:self updateTime:self.time];
+    }
+
+}
+-(void)setTimeSpeed:(CGFloat)timeSpeed{
+    
+    self.timeSpeed = MAX(timeSpeed, 0);
+    
+    if (timeSpeed == 0) {
+        [self pause];
+    }
+}
+                  
+-(void)start{
+    if (!_clock) {
+        [self initClock];
+    }
+    
+    if (self.launched) {
+        _timeSpeed = _pausedTimeSpeed;
+    } else {
+         _previousDate = CACurrentMediaTime();
+         _timeSpeed = 1.0f;
+        [_clock addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+        self.launched = YES;
+        self.paused = NO;
+    }
+}
+
+-(void)stop{
+    if (_clock) {
+        [_clock invalidate];
+        _clock = nil;
+    }
+    self.launched = NO;
+}
+
+-(void)pause{
+    //  暂停
+    if (!self.paused) {
+        _pausedTimeSpeed = _timeSpeed;
+        //  更新时间速度
+        _timeSpeed = 0;
+        self.paused = YES;
+    }
+}
+
+-(void)resume{
+    if (self.paused) {
+        _timeSpeed = _pausedTimeSpeed;
+        self.paused = NO;
+    }
+}
+/// 更新逻辑时间系统
+- (void)updateTime{
+    
+    CFTimeInterval currentDate = CACurrentMediaTime();
+    self.time += (currentDate - _previousDate) * self.timeSpeed;
+    _previousDate = currentDate;
+}
 
 @end
